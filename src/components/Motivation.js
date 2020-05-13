53b1509caeeeae8e50d83757a2db6645e3c91b00
amirfahoum <amirfahoum@gmail.com>
2020-05-13 10:58:43 +0300

    set Time to death +merge with others files

--- src/components/Motivation.js
+++ src/components/Motivation.js
@@ -1,32 +1,33 @@
 import React, { useEffect } from "react";
-var src1="/home/amir/repo01/projects/Tamagotchi/public/22220.png"
+var src1 = "/home/amir/repo01/projects/Tamagotchi/public/22220.png"
 
-var maxtime =30;
-var dividby6=6;
 const Motivation = props => {
+    const [Cegaret, setCegaret] = React.useState([])
+    const handleCegaretChange = (number) => {
+        var arr = [];
+        for (let i = 0; i < number; i++) {
+            arr.push("");
+        }
+        setCegaret(arr);
+    }
+    React.useEffect(() => {
+        console.log("hii")
+        handleCegaretChange(props.timeToDeath / 6);
 
+    }, [props.timeToDeath]);
 
-    
-   const[Cegaret,setCegaret]= React.useState(["","","","","",""])
-   React.useEffect(() => {
-       setInterval(() => {
-           setCegaret(previousCegaret=> previousCegaret.slice(0,-1))
-           
-       }, 1000);
-
-  }, []);
-return (
-    <div>
-    <form onSubmit={(e)=> {e.preventDefault();}}>
-      <fieldset>
+    return (
         <div>
-        <h2>Motivation:</h2>
-       {Cegaret.map(()=><img src={src1}  />)}
+            <form onSubmit={(e) => { e.preventDefault(); }}>
+                <fieldset>
+                    <div>
+                        <h2>Motivation:</h2>
+                        {Cegaret.map(() => <img src={src1} />)}
+                    </div>
+                </fieldset>
+            </form>
         </div>
-      </fieldset>
-    </form>
-  </div>
-);
+    );
 }
 
 export default Motivation;
--- src/components/app.js
+++ src/components/app.js
@@ -3,7 +3,7 @@ import UserHeader from './UserHeader';
 import Motivation from './Motivation'
 
 export default function App() {
-  const [timeToDeath, setTimeToDeath] = React.useState(30);
+  const [timeToDeath, setTimeToDeath] = React.useState(6);
   const [searchText,setSearchText] = React.useState("");
   const handleChangeTime = event => setTimeToDeath(event.target.value);
   const handleChangeText  = event => setSearchText(event.target.value);
