144bf2df399ef8f227e2e7ca789dd10a05520c8b
fridabu <f0504070002@gmail.com>
2020-05-14 14:08:04 +0300

    #8

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import weed from '../../public/weed.png';
-import flower from '../../public/flower.png';
 import love from '../../public/love.png';
 import cookie from '../../public/cookie.png';
 
